0e788d639f4a54b997c2264f4bb67ca77b4de6e3
kirschentraum <mukaikozue1984@gmail.com>
2021-10-08 09:14:04 +0800

    commit

--- Project4/Project4/1.cpp
+++ Project4/Project4/1.cpp
@@ -1,69 +1,13 @@
-#include<Windows.h>
-#include<math.h>
+#include<cstdio>
 
-#define NUM 100000
-#define TWOPI (2*3.14159265359)
-
-POINT apt[NUM];
-
-LRESULT CALLBACK WndProc(_In_ HWND hwnd, _In_ UINT message, _In_ WPARAM wParam, _In_ LPARAM lParam)
+void func(int a, int b)
 {
-	static int cxClient, cyClient;
-	HDC hdc;
-	PAINTSTRUCT ps;
-	int i;
-
-switch (message)
-	{
-	case WM_SIZE:
-		cxClient = LOWORD(lParam);
-		cyClient = HIWORD(lParam);
-		return 0;
-	case WM_PAINT:
-		hdc = BeginPaint(hwnd, &ps);
-		MoveToEx(hdc, 0, cyClient / 2, NULL);
-		LineTo(hdc, cxClient, cyClient / 2);
-		for (i = 0; i < NUM; i++)
-		{
-			apt[i].x = i * cxClient / NUM;
-			apt[i].y = (int)(cyClient / 2 * (1 - sin(TWOPI * i / NUM)));
-		}
-		Polyline(hdc, apt, NUM);
-		EndPaint(hwnd, &ps);
-		return 0;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	}
-	return DefWindowProcW(hwnd, message, wParam, lParam);
+	printf("%d\n%d\n", a, b);
 }
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+
+int main()
 {
-	wchar_t appName[] = L"SineWave";
-	HWND hwnd;
-	MSG msg;
-	WNDCLASSW wndclass = {};
-	wndclass.style = CS_HREDRAW | CS_VREDRAW;
-	wndclass.lpfnWndProc = WndProc;
-	wndclass.cbClsExtra = 0;
-	wndclass.cbWndExtra = 0;
-	wndclass.hInstance = hInstance;
-	wndclass.hIcon = LoadIconW(NULL, IDI_APPLICATION);
-	wndclass.hCursor = LoadCursorW(NULL, IDC_ARROW);
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndclass.lpszMenuName = NULL;
-	wndclass.lpszClassName = appName;
-	if (!RegisterClassW(&wndclass))
-	{
-		MessageBoxW(NULL, L"This Program requires Windows NT!", appName, MB_ICONERROR);
-		return 0;
-	}
-	hwnd = CreateWindowW(appName, L"Get System Metrics No.1", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
-	ShowWindow(hwnd, nShowCmd);
-	while (GetMessageW(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessageW(&msg);
-	}
-	return msg.wParam;
+	int c = 25;
+	func(++c, c);
+	return 0;
 }
